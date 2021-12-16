d87a4fbb05c02cebb25d50b9f3be674516705087
chefKold <j.koldenhof@outlook.com>
2021-12-16 11:44:21 +0100

    Course associative attributes were not named equally to the class diagram.

--- src/domain/Course.java
+++ src/domain/Course.java
@@ -9,16 +9,16 @@ public class Course {
     private String topic;
     private String discription;
     private Difficulty difficulty;
-    private List<Module> courseModules;
-    private List<Course> aanbevolenCourses;
+    private List<Module> modules;
+    private List<Course> recommendedCourses;
 
     public Course(String name, String topic, String discription, Difficulty difficulty) {
         this.name = name;
         this.topic = topic;
         this.discription = discription;
         this.difficulty = difficulty;
-        this.courseModules = new ArrayList<>();
-        this.aanbevolenCourses = new ArrayList<>();
+        this.modules = new ArrayList<>();
+        this.recommendedCourses = new ArrayList<>();
     }
 
     public String getName() {
@@ -37,20 +37,20 @@ public class Course {
         return difficulty;
     }
 
-    public List<Modules> getCourseModules() {
-        return courseModules;
+    public List<Module> getModules() {
+        return modules;
     }
 
-    public List<Course> getAanbevolenCourses() {
-        return aanbevolenCourses;
+    public List<Course> getRecommendedCourses() {
+        return this.recommendedCourses;
     }
 
-    public void setAanbevolenCourses(List<Course> aanbevolenCourses) {
-        this.aanbevolenCourses = aanbevolenCourses;
+    public void setRecommendedCourses(List<Course> recommendedCourses) {
+        this.recommendedCourses = recommendedCourses;
     }
 
-    public void setCourseModules(List<Modules> courseModules) {
-        this.courseModules = courseModules;
+    public void setModules(List<Module> modules) {
+        this.modules = modules;
     }
 
     public void setDifficulty(Difficulty difficulty) {
@@ -69,6 +69,6 @@ public class Course {
     public String toString() {
         // TODO Auto-generated method stub
         return String.format("Name: %s, Topic: %s, Discription: %s, Difficulty: %s\n%s\n \n%s\n  ", name, topic,
-                discription, this.difficulty, this.courseModules, this.aanbevolenCourses);
+                discription, this.difficulty, this.modules, this.recommendedCourses);
     }
 }
