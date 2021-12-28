5612a2da6c61f9cbf7b995364e902bc9f04e4835
Jascha van der Ark <jem.vanderark@student.avans.nl>
2021-12-28 14:56:33 +0100

    Updated SQL script
    Co-authored-by: N1TROGUE <N1TROGUE@users.noreply.github.com>

--- src/database/ModuleRepository.java
+++ src/database/ModuleRepository.java
@@ -5,6 +5,8 @@ import java.time.LocalDate;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.HashMap;
+
 import domain.Module;
 import domain.Status;
 
@@ -104,7 +106,7 @@ public class ModuleRepository extends Repository {
                 }
                 // Updates the relevant tables in the database
                 statement.executeUpdate("UPDATE ContentItem SET Description = '" + description + "', Status = '" + status + "' WHERE ContentID = " + contentID + "");
-                statement.executeUpdate("UPDATE Module SET ContactEmail = '" + contactEmail + "', OrderNumber = " + trackingNumber + " WHERE ContentID = " + contentID + "");
+                statement.executeUpdate("UPDATE Module SET ContactEmail = '" + contactEmail + "', PositionInCourse = " + trackingNumber + " WHERE ContentID = " + contentID + "");
             } catch (SQLException e) {
                 e.printStackTrace();
             }
@@ -137,8 +139,7 @@ public class ModuleRepository extends Repository {
     public ArrayList<Object> retrieve() {
         try {
             Statement statement = this.connection.getConnection().createStatement();
-            ResultSet result = statement.executeQuery(
-                    "SELECT * FROM Module JOIN ContentItem ON Module.ContentID = ContentItem.ContentID JOIN Contact ON Module.ContactEmail = Contact.Email");
+            ResultSet result = statement.executeQuery("SELECT * FROM Module JOIN ContentItem ON Module.ContentID = ContentItem.ContentID JOIN Contact ON Module.ContactEmail = Contact.Email");
             ArrayList<Object> modules = new ArrayList<>();
 
             while (result.next()) {
@@ -146,7 +147,7 @@ public class ModuleRepository extends Repository {
                 Status status = Status.valueOf(result.getString("Status"));
                 String title = result.getString("Title");
                 int version = result.getInt("Version");
-                int trackingNumber = result.getInt("OrderNumber");
+                int trackingNumber = result.getInt("PositionInCourse");
                 String description = result.getString("Description");
                 String contactName = result.getString("Name");
                 String emailAddress = result.getString("Email");
@@ -156,6 +157,86 @@ public class ModuleRepository extends Repository {
 
             return modules;
 
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+
+    public HashMap<String, Integer> getAllModuleNames(){
+        try {
+            
+            Statement statement = this.connection.getConnection().createStatement();
+            ResultSet resultSet = statement.executeQuery("SELECT Title, Version ,ContentID FROM Module");
+            HashMap<String, Integer> modules = new HashMap<>();
+            while(resultSet.next()) {
+                String key = resultSet.getString("Title") + " (Versie: " + resultSet.getInt("Version") + ")";
+                System.out.println(key);
+                modules.put(key, resultSet.getInt("ContentID"));
+            }
+            return modules;
+
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+
+
+    public Module retrieveModuleByID(int ID) {
+        LocalDate date = null;
+        Status status = null;
+        String title = "";
+        int version = 0;
+        int positionWithinCourse = 0;
+        String description = "";
+        String contactName = "";
+        String emailAddress = "";
+
+        
+        try {
+        Statement statement = this.connection.getConnection().createStatement();
+        ResultSet moduleRetrieve = statement.executeQuery("SELECT * FROM Module WHERE ContentID = "+ID+"");
+
+        while(moduleRetrieve.next()) {
+            title = moduleRetrieve.getString("Title");
+            version = moduleRetrieve.getInt("Version");
+            positionWithinCourse = moduleRetrieve.getInt("PositionInCourse");
+            emailAddress = moduleRetrieve.getString("ContactEmail");
+        }
+
+        try {
+            statement = this.connection.getConnection().createStatement();
+            ResultSet contentItemRetrieve = statement.executeQuery("SELECT CreationDate, Status FROM ContentItem WHERE ContentID = "+ID+"");
+            
+            while(contentItemRetrieve.next()) {
+                date = LocalDate.parse(contentItemRetrieve.getString("CreationDate"));
+                status = Status.valueOf(contentItemRetrieve.getString("Status"));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+
+
+        try {
+            statement = this.connection.getConnection().createStatement();
+            ResultSet contactRetrieve = statement.executeQuery("SELECT * FROM Contact WHERE Email = '"+emailAddress+"'");
+            
+            while(contactRetrieve.next()) {
+                contactName = contactRetrieve.getString("Name");
+            }
+            
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+
+        Module module = new Module(date, status, title, version, positionWithinCourse, description, contactName, emailAddress);
+        return module;
         } catch (SQLException e) {
             e.printStackTrace();
             return null;
