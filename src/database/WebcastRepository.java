25140814c3437a7a8e9f966e606c4fc976c26e51
Jascha van der Ark <jem.vanderark@student.avans.nl>
2021-12-31 16:05:15 +0100

    Created WebcastRepository class

---
+++ src/database/WebcastRepository.java
@@ -0,0 +1,44 @@
+package database;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import domain.Webcast;
+
+import domain.Status;
+
+public class WebcastRepository extends Repository {
+
+
+    //Creates a new repository, gives connection to database.
+    public WebcastRepository() {
+     super();
+    } 
+
+   @Override
+   public ArrayList retrieve() {
+       // TODO Auto-generated method stub
+       return null;
+   }
+
+   @Override
+   public void update(Object domainObject) {
+       // TODO Auto-generated method stub
+       
+   }
+
+   @Override
+   public void delete(Object domainObject) {
+       // TODO Auto-generated method stub
+       
+   }
+
+   @Override
+   public void insert(Object domainObject) {
+       // TODO Auto-generated method stub
+       
+   }
+    
+}
