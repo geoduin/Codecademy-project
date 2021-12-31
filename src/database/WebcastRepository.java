b03ca7f0f96307228d7bb1b95f0d17e0292850e2
Jascha van der Ark <jem.vanderark@student.avans.nl>
2021-12-31 16:54:01 +0100

    Made getters and setters for Webcast and reset WebcastReposityory

--- src/database/WebcastRepository.java
+++ src/database/WebcastRepository.java
@@ -1,61 +1,31 @@
 package database;
 
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.ArrayList;
-import domain.Webcast;
 
-import domain.Status;
+public class WebcastRepository extends Repository{
 
-public class WebcastRepository extends Repository {
+    @Override
+    public void insert(Object domainObject) {
+        // TODO Auto-generated method stub
+        
+    }
 
+    @Override
+    public void update(Object domainObject) {
+        // TODO Auto-generated method stub
+        
+    }
 
-    //Creates a new repository, gives connection to database.
-    public WebcastRepository() {
-     super();
-    } 
+    @Override
+    public void delete(Object domainObject) {
+        // TODO Auto-generated method stub
+        
+    }
 
- //Retrieves all webcasts from the database
- @Override
-
- //Initializing needed variables
- public ArrayList<String> retrieve() {
-
-    //Returns the (unique) title of all webcasts saved in the database
-     try {
-        Statement statement = this.connection.getConnection().createStatement();
-        ResultSet result = statement.executeQuery("SELECT Title FROM Webcast");
-        ArrayList<String> webcasts = new ArrayList<>();
-        while(result.next()) {
-            webcasts.add(result.getString("Title"));
-        }
-        return webcasts;
-         
-     } catch (SQLException e) {
-         e.printStackTrace();
-         return null;
-     }
-     
- }
-
-   @Override
-   public void update(Object domainObject) {
-       // TODO Auto-generated method stub
-       
-   }
-
-   @Override
-   public void delete(Object domainObject) {
-       // TODO Auto-generated method stub
-       
-   }
-
-   @Override
-   public void insert(Object domainObject) {
-       // TODO Auto-generated method stub
-       
-   }
+    @Override
+    public ArrayList retrieve() {
+        // TODO Auto-generated method stub
+        return null;
+    }
     
 }
