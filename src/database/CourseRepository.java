62d214a736a438b1fc3c699d547a5bd630c69bfa
chefKold <j.koldenhof@outlook.com>
2022-01-01 13:49:26 +0100

    Refactor in Rep. & ModuleRep. + added CourseRep.

---
+++ src/database/CourseRepository.java
@@ -0,0 +1,31 @@
+package database;
+
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+import domain.Course;
+
+public class CourseRepository extends Repository<Course> {
+
+    @Override
+    public void insert(Course course) {
+
+    }
+
+    @Override
+    public void update(Course course) {
+
+    }
+
+    @Override
+    public void delete(Course course) {
+
+    }
+
+    @Override
+    public ArrayList<Course> retrieve() {
+        return null;
+    }
+
+}
--- src/database/Repository.java
+++ src/database/Repository.java
@@ -1,4 +1,5 @@
 package database;
+
 import java.util.ArrayList;
 
 public abstract class Repository<T> {
@@ -6,17 +7,16 @@ public abstract class Repository<T> {
     DBConnection connection;
 
     public Repository() {
-        //Default URL "jdbc:sqlserver://localhost;databaseName=Codecademy;integratedSecurity=true;" 
+        // Default URL
+        // "jdbc:sqlserver://localhost;databaseName=Codecademy;integratedSecurity=true;"
         this.connection = new DBConnection();
     }
 
+    public abstract void insert(T domainObject);
 
-    
-    abstract public void insert(T domainObject);
+    public abstract void update(T domainObject);
 
-    abstract public void update(T domainObject);
+    public abstract void delete(T domainObject);
 
-    abstract public void delete(T domainObject);
-
-    abstract public ArrayList<T> retrieve();
+    public abstract ArrayList<T> retrieve();
 }
