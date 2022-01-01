7e51145fbdf52e9926f8c023bb4adedcc2b3a318
N1TROGUE <mohammedbogatyrev@gmail.com>
2022-01-01 17:53:40 +0100

    Removed faulty method (checkCourseCompletion) from CourseRepository class

--- src/database/CourseRepository.java
+++ src/database/CourseRepository.java
@@ -40,32 +40,4 @@ public class CourseRepository extends Repository<Course> {
         // TODO Auto-generated method stub
         return null;
     }    
-
-    public boolean courseCompletionCheck(Course domainObject) {
-        String name = domainObject.getName();
-        int completedModules = 0;
-        int uncompletedModules = 0;
-
-        try {
-            Statement statement = this.connection.getConnection().createStatement();
-            ResultSet resultSetCompletedModules = statement.executeQuery("SELECT COUNT(ContentItem.Status) AS 'Modules' FROM Module JOIN ContentItem ON Module.ContentID = ContentItem.ContentID WHERE Module.CourseName = '"+name+"' AND ContentItem.Status LIKE 'COMPLETED'");
-            ResultSet resultSetUncompletedModules = statement.executeQuery("SELECT COUNT(ContentItem.Status) AS 'Modules 'FROM Module JOIN ContentItem ON Module.ContentID = ContentItem.ContentID WHERE Module.CourseName = '"+name+"' AND ContentItem.Status NOT LIKE 'COMPLETED'");
-            
-            while (resultSetCompletedModules.next()) {
-                completedModules = resultSetCompletedModules.getInt("Modules");
-            }
-
-            while (resultSetUncompletedModules.next()) {
-                uncompletedModules = resultSetUncompletedModules.getInt("Modules");
-            }  
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-
-        if (completedModules == uncompletedModules) {
-            return true;
-        } else {
-            return false;
-        }
-    }
 }
