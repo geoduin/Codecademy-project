84070f5c0a38f70852640862424dfcf800e144f5
Jascha van der Ark <jem.vanderark@student.avans.nl>
2022-01-09 16:18:43 +0100

    Created StatisticsView, fixed miscellaneous changes
    Co-authored-by: N1TROGUE <mohammedbogatyrev@gmail.com>

---
+++ src/gui/StatisticsViews.java
@@ -0,0 +1,19 @@
+package gui;
+
+public class StatisticsViews extends View{
+
+
+    public StatisticsViews(GUI baseUI) { 
+        super(baseUI);
+    }
+
+    @Override
+    protected void createView() {
+
+        
+        
+        
+        
+    }
+    
+}
