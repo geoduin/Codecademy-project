dc5b9436b359f529f71e349cba2b3fa91089f1ab
chefKold <j.koldenhof@outlook.com>
2022-01-16 19:40:19 +0100

    ContenItem comments added.

--- src/domain/ContentItem.java
+++ src/domain/ContentItem.java
@@ -2,18 +2,22 @@ package domain;
 
 import java.time.LocalDate;
 
-//CLass responsible for defining the overarching attributes of Webcasts and Modules
+/* 
+*CLass responsible for defining the overarching attributes of Webcasts and Modules. The database *ContentID is *also an attribute, because in GUI editing functionality it is efficient to use to push *changes to the database (mainly because module has a multi part primary key) 
+*/
 public abstract class ContentItem {
     private LocalDate publicationDate;
     private Status status;
     private int contentID;
 
-    public ContentItem(LocalDate date, Status status, int contentID) {
+    // Self explanatory constructor
+    ContentItem(LocalDate date, Status status, int contentID) {
         this.publicationDate = date;
         this.status = status;
         this.contentID = contentID;
     }
 
+    // Getters and setters bulk
     public LocalDate getDate() {
         return this.publicationDate;
     }
