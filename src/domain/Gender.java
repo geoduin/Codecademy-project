084c17e34282f3228152a48810bc49f1eec291d5
chefKold <j.koldenhof@outlook.com>
2022-01-16 21:28:18 +0100

    Added comments to the enums.

--- src/domain/Gender.java
+++ src/domain/Gender.java
@@ -1,14 +1,41 @@
 package domain;
 
+//Gender  constants, used for student type objects
 public enum Gender {
-    M{@Override
-    public String toString() {
-        return "Male";
-    }}, F{@Override
-    public String toString() {
-        return "Female";
-    }}, O { @Override
-    public String toString() {
-        return "Other";
-    }}
+    M {
+        /*
+         * For a beter user experience, casting the full string representation of *the M
+         * constant. toString is used, so that ComboBoxes<Gender> can still be
+         * applied. The .Name() method is used whenever the actual constant needs * * to
+         * be used (for example when creating a student)
+         */
+        @Override
+        public String toString() {
+            return "Male";
+        }
+    },
+    F {
+        /*
+         * For a beter user experience, casting the full string representation of *the F
+         * constant. toString is used, so that ComboBoxes<Gender> can still be
+         * applied. The .Name() method is used whenever the actual constant needs * * to
+         * be used (for example when creating a student)
+         */
+        @Override
+        public String toString() {
+            return "Female";
+        }
+    },
+    O {
+        /*
+         * For a beter user experience, casting the full string representation of *the O
+         * constant. toString is used, so that ComboBoxes<Gender> can still be
+         * applied. The .Name() method is used whenever the actual constant needs * * to
+         * be used (for example when creating a student)
+         */
+        @Override
+        public String toString() {
+            return "Other";
+        }
+    }
 }
--- src/domain/Status.java
+++ src/domain/Status.java
@@ -1,5 +1,6 @@
 package domain;
 
+//Difficulty constants, used for content items
 public enum Status {
     CONCEPT, ACTIVE, ARCHIVED
 }
