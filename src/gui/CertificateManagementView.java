3cfec3ef87e89ae088f0aacd130659fe94bbf81d
N1TROGUE <mohammedbogatyrev@gmail.com>
2022-01-16 02:18:41 +0100

    Created create, edit, and delete functionality in gui.CertificateManagementView

--- src/gui/CertificateManagementView.java
+++ src/gui/CertificateManagementView.java
@@ -1,5 +1,6 @@
 package gui;
 
+import domain.Certificate;
 import domain.Enrollment;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
@@ -20,15 +21,28 @@ public class CertificateManagementView extends View {
 
     @Override
     public void createView() {
-        // Initializing view (layout)
+        // Layout positioning elements
         GridPane view = generateGrid();
 
-        // Main UI components
-        Label mainLabel = new Label("Enrollments that are eligible for a certificate:");
+        //  UI Components
+        Label enrollmentsLabel = new Label("Eligible enrollments");
         ComboBox<Enrollment> enrollmentDropdown = new ComboBox<>();
-        Button createCertificateBtn = new Button("Create certificate for selected enrollment");
+        Button createCertificateButton = new Button("Create");
+        Label certificatesLabel = new Label("Manage certificates");
+        ComboBox<Certificate> certificateDropdown = new ComboBox<>();
+        Button editButton = new Button("Edit");
+        Button deleteButton = new Button("Delete");
 
-        // Actual setup of the dropdown containing eligible enrollments setup
+        //  Further layout setup
+        view.add(enrollmentsLabel, 0, 0);
+        view.add(enrollmentDropdown, 0, 1);
+        view.add(createCertificateButton, 0, 2);
+        view.add(certificatesLabel, 0, 3);
+        view.add(certificateDropdown, 0, 4);
+        view.add(editButton, 0, 5);
+        view.add(deleteButton, 0, 6);
+
+        //  Setup of the enrollmentDropdown containing eligible enrollments setup
         // Adding enrollment instances to the dropdown
         int count = 0;
         for (Enrollment enrollment : new EnrollmentLogic().getEnrollmentsEligibleForCertificate()) {
@@ -38,57 +52,91 @@ public class CertificateManagementView extends View {
         enrollmentDropdown.setPromptText(count + " enrollment(s) available");
 
         // Giving the user an option to create a certificate with the selected
-        // enrollment from the dropdown
-        createCertificateBtn.setOnMousePressed(clicked -> {
+        // enrollment from the EnrollmentsDropdown
+        createCertificateButton.setOnMousePressed(clicked -> {
             if (enrollmentDropdown.getValue() == null) {
                 return;
             }
             addCertificateView(enrollmentDropdown.getValue());
         });
 
-        // Finalization and activation of view
-        view.add(mainLabel, 0, 0);
-        view.add(enrollmentDropdown, 0, 1);
-        view.add(createCertificateBtn, 0, 2);
+        editButton.setOnMouseClicked(clicked -> {
+            if (certificateDropdown.getValue() == null) {
+                return;
+            }
+            editCertificateView(certificateDropdown.getValue());
+        });
 
-        this.gui.goToNext(view, "Select enrollment eligible for certificate creation");
+        deleteButton.setOnMouseClicked(clicked -> {
+            if (certificateDropdown.getValue() == null) {
+                return;
+            }
+            
+            certificateLogic.deleteCertificate(certificateDropdown.getValue());
+            certificateSuccessfullyDeleted();
+        });
+
+        //  Adding certificates to certificateDropdown
+        certificateDropdown.getItems().addAll(certificateLogic.retrieveAllCertificates());
+        certificateDropdown.setPromptText(certificateLogic.retrieveAllCertificates().size() + " certificate(s) available");
+
+        this.gui.goToNext(view, "Manage certificates");
+    }
+
+    public void certificateSuccessfullyDeleted() {
+        // Layout positioning elements
+        GridPane view = generateGrid();
+
+        // UI components
+        Label label = new Label("Successfully deleted certificate!");
+        Button homeBtn = new Button("Home");
+        Button addAnotherCertificateBtn = new Button("Delete");
+        
+        // Further layout setup
+        view.add(label, 1, 0);
+        view.add(homeBtn, 0, 1);
+        view.add(addAnotherCertificateBtn, 2, 1);
+
+        // Behavior of UI components
+        homeBtn.setOnMouseClicked(clicked -> new HomeView(this.gui).createView());
+
+        this.gui.goToNext(view, "Successfully deleted!");
     }
 
     public void addCertificateView(Enrollment enrollment) {
         // Layout positioning elements
         GridPane view = generateGrid();
-        
 
-        // UI components
+        
         Label nameLabel = new Label("Name");
         Label gradeLabel = new Label("Grade");
         TextField nameTextField = new TextField();
-        Label incorrectNameTextField = new Label();
+        Label incorrectNameLabel = new Label();
         TextField gradeTextField = new TextField();
-        Label incorrectGradeTextField = new Label();
+        Label incorrectGradeLabel = new Label();
         Button submitButton = new Button("Submit");
 
         // Further layout setup
         view.add(nameLabel, 0, 0);
         view.add(nameTextField, 0, 1);
-        view.add(incorrectNameTextField, 1, 1);
+        view.add(incorrectNameLabel, 1, 1);
         view.add(gradeLabel, 0 , 2);
         view.add(gradeTextField, 0 , 3);
-        view.add(incorrectGradeTextField, 1, 3);
+        view.add(incorrectGradeLabel, 1, 3);
         view.add(submitButton, 0, 4);
 
-        // Checking input
+        // Checking if input is correct
         submitButton.setOnMouseClicked(clicked -> {
             String employeeName = nameTextField.getText();
             int grade = Integer.valueOf(gradeTextField.getText());
 
             if (!employeeName.chars().allMatch(Character::isLetter) || nameTextField.getText().isBlank()) {
-                incorrectNameTextField.setText("Please enter a valid name");
+                incorrectNameLabel.setText("Please enter a valid name");
                 return;
             } 
 
             if (!InputValidation.isValidGrade(grade) || gradeTextField.getText().isBlank()) {
-                incorrectGradeTextField.setText("Please enter a number between 1-10");
+                incorrectGradeLabel.setText("Please enter a number between 1-10");
                 return;
             }
 
@@ -114,5 +162,72 @@ public class CertificateManagementView extends View {
         homeBtn.setOnMouseClicked(clicked -> new HomeView(this.gui).createView());
 
         this.gui.goToNext(view, "Successfully added!");
+    }
+
+    public void editCertificateView(Certificate certificate) {
+        // Layout positioning elements
+        GridPane view = generateGrid();
+
+        // UI components
+        Label editCertificateLabel = new Label("Edit certificate");
+        Label nameLabel = new Label("Name: ");
+        TextField nameTextField = new TextField(certificate.getNameOfIssuer());
+        Label incorrectNameLabel = new Label();
+        Label gradeLabel = new Label("Grade: ");
+        TextField gradeTextField = new TextField(String.valueOf(certificate.getGrade()));
+        Label incorrectGradeLabel = new Label();
+        Button editButton = new Button("Edit");
+
+        // Further layout setup
+        view.add(editCertificateLabel, 0, 0);
+        view.add(nameLabel, 0, 1);
+        view.add(nameTextField, 1, 1);
+        view.add(incorrectNameLabel, 2, 1);
+        view.add(gradeLabel, 0, 2);
+        view.add(gradeTextField, 1 ,2);
+        view.add(incorrectGradeLabel, 2, 2);
+        view.add(editButton, 0, 3);
+
+        editButton.setOnMouseClicked(clicked -> {
+            String employeeName = nameTextField.getText();
+            int grade = Integer.valueOf(gradeTextField.getText());
+
+            if (!employeeName.chars().allMatch(Character::isLetter) || nameTextField.getText().isBlank()) {
+                incorrectNameLabel.setText("Please enter a valid name");
+                return;
+            }
+
+            if (!InputValidation.isValidGrade(grade) || gradeTextField.getText().isBlank()) {
+                incorrectGradeLabel.setText("Please enter a number between 1-10");
+                return;
+            }
+
+            certificate.setNameOfIssuer(employeeName);
+            certificate.setGrade(grade);
+            certificateLogic.updateCertificate(certificate);
+            certificateSuccessfullyUpdated();
+        });
+        
+        this.gui.goToNext(view, "Edit certificate");
+    }
+
+    public void certificateSuccessfullyUpdated() {
+        // Layout positioning elements
+        GridPane view = generateGrid();
+
+        // UI components
+        Label label = new Label("Successfully updated certificate!");
+        Button homeBtn = new Button("Home");
+        Button addAnotherCertificateBtn = new Button("Update");
+
+        // Further layout setup
+        view.add(label, 1, 0);
+        view.add(homeBtn, 0, 1);
+        view.add(addAnotherCertificateBtn, 2, 1);
+
+        // Behavior of UI components
+        homeBtn.setOnMouseClicked(clicked -> new HomeView(this.gui).createView());
+
+        this.gui.goToNext(view, "Successfully updated!");
     }
 }
